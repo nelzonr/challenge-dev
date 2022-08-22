7c6f55a9de651bce82ec3437b0ce072e1488d7b8
nelzonr <nkramirez@hotmail.com>
2022-08-22 04:31:55 -0300

    Postgre update engine

--- database.py
+++ database.py
@@ -8,10 +8,7 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
