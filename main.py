0221be019940347717a1a7fd8c3997fe712599e0
nelzonr <nkramirez@hotmail.com>
2022-08-21 22:43:42 -0300

    endpoint to upload files an update .gitignore

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ app = FastAPI()
 @app.get("/")
 def hello_world():
     return {"message": "Hello World"}
+
+@app.post("/upload")
+async def upload_files(files: List[UploadFile]):
+    output = []
+    for file in files:
+        output.append({"filename": file.filename, "contents": await file.read()})
+
+    return {"output": output}
