a8fa48eee06395add7c445f3d5c7ce8a30f1b751
nelzonr <nkramirez@hotmail.com>
2022-08-22 00:20:55 -0300

    added endpoint list and read from Tipos

--- CNAE/main.py
+++ CNAE/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, UploadFile
+from fastapi import FastAPI, Depends, UploadFile, HTTPException
 from typing import List
 from sqlalchemy.orm import Session
 from database import engine, SessionLocal
@@ -8,6 +8,7 @@ app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
+
 def get_db():
     try:
         db = SessionLocal()
@@ -15,6 +16,21 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/")
+
+@app.get("/tipos")
 async def read_all(db: Session = Depends(get_db)):
-    return db.query(models.Todos).all()
+    return db.query(models.Tipos).all()
+
+
+@app.get("/tipos/{tipo_id}")
+async def read_tipo(tipo_id: int, db: Session = Depends(get_db)):
+    tipo_model = db.query(models.Tipos)\
+        .filter(models.Tipos.id == tipo_id)\
+        .first()
+    if tipo_model is not None:
+        return tipo_model
+    raise http_exception()
+
+
+def http_exception(status_code=404, message="Not found"):
+    return HTTPException(status_code=status_code, detail=message)
