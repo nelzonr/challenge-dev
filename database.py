428dc8884253c5a62f3d57d51767474ef2bf93e3
nelzonr <nkramirez@hotmail.com>
2022-08-22 03:38:13 -0300

    added support to PostgreSQL

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "sqlite:///./database.db"
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(
     DATABASE_URL,
